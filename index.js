715cb961c0e967f743061e05ad1abf49addd65b8
xEfren548 <efren.alvarez@iteso.mx>
2024-02-27 18:44:40 -0600

    some changes

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
 
-const eventRoutes = require('./routes/eventRoutes'); 
+const eventRoutes = require('./routes/eventRoutes');
 const habitacionesRoutes = require('./routes/habitacionesRoutes');
 // Configura Express para servir archivos estáticos desde la carpeta 'public'
 
 app.use(express.static(path.join(__dirname, 'src', 'public')));
 app.use(express.json())
+
+// Configuración del motor de plantillas
+app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs.engine({
+    extname: '.hbs',
+    // Disable the knownHelpersOnly check
+    runtimeOptions: {
+        knownHelpersOnly: false,
+        allowProtoPropertiesByDefault: true
+    }
+}))
+app.set('view engine', '.hbs');
 
 
 
@@ -23,7 +36,7 @@ async function connect() {
         app.listen(port, () => {
             console.log(`App is running on port ${port}`);
         })
-    
+
     } catch (err) {
         console.log('failed to connect' + err.message);
     }
