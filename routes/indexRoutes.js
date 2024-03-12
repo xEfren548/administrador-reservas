22e1d770648953129649a3016363f9ebe0262f32
xEfren548 <efren.alvarez@iteso.mx>
2024-03-12 16:30:40 -0600

    servicios adicionales

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const eventRoutes = require('./eventRoutes');
 const habitacionesRoutes = require('./habitacionesRoutes');
 const userRoutes = require('./userRoutes');
+const serviciosRoutes = require('./serviciosRoutes');
 
 router.get('/', (req, res) => {
     res.render('index'); 
@@ -12,6 +13,7 @@ router.get('/', (req, res) => {
 router.use('/api', eventRoutes);
 router.use('/api', habitacionesRoutes);
 router.use('/api', userRoutes);
+router.use('/api', serviciosRoutes);
 
 
 module.exports = router;
