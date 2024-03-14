18dc945ae4870baebf646a0cacdccdb859cb96af
xEfren548 <efren.alvarez@iteso.mx>
2024-03-14 12:02:00 -0600

    requires folder added

---
+++ src/public/requires/js/pages/data-table.js
@@ -0,0 +1,24 @@
+//[Data Table Javascript]
+$(function () {
+    "use strict";
+
+    $('#example').DataTable();
+    
+	$('#example1').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    });
+	
+	
+	$('#example').DataTable( {
+		dom: 'Bfrtip',
+		buttons: [
+			'copy', 'csv', 'excel', 'pdf', 'print'
+		]
+	} );
+	
+  }); // End of use strict
---
+++ src/public/requires/js/pdfmake-0_1_38/index.php
@@ -0,0 +1,47 @@
+﻿<!doctype html>
+<html lang='en'>
+<head>
+  <meta charset='utf-8'>
+  <title>my first pdfmake example</title>
+  <script src='build/pdfmake.min.js'></script> 
+  <script src='build/vfs_fonts.js'></script> 
+</head>
+
+<body>
+
+	<button type="button" onclick="creaPDF()">Crear PDF</button>
+
+</body>
+
+</html>
+
+<script type="text/javascript">
+
+			
+		
+
+
+	function creaPDF()
+	{
+
+var docDefinition = {
+		content: [
+	'pdfmake (since it\'s based on pdfkit) supports JPEG and PNG format',
+	'If no width/height/fit is provided, image original size will be used',
+	
+	'If you specify width, image will scale proportionally',
+	
+	'If you specify both width and height - image will be stretched',
+	
+	
+	'Images can be also provided in dataURL format...',
+	'or be declared in an "images" dictionary and referenced by name',
+],
+
+};
+			
+		pdfMake.createPdf(docDefinition).download('Report.pdf');
+		
+	}//close function
+	
+</script>
