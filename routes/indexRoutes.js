d40427216ff02056dce06a519ba08578cdeea60d
Juanfe <juanfegenio@hotmail.com>
2024-04-10 17:07:32 -0600

    correccion de sidebar

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -34,10 +34,10 @@ router.use('/login', loginRoute);
 router.use("/api", authRoutes);
 
 // Validating user's token in later requests.
-//router.use(currentuser);
+router.use(currentuser);
 
 // Determining user access based on privileges.
-//router.use(userPrivilege);
+router.use(userPrivilege);
 
 // Use middlewares.
 router.use('/', instruccionesUsuario);
