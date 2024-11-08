ced9fa8cb31a7e44e0bb0b9f762064822a285ae0
xEfren548 <efren.alvarez@iteso.mx>
2024-11-08 10:38:55 -0600

    fix en instrucciones de usuario de fechas

--- controllers/instructionsController.js
+++ controllers/instructionsController.js
@@ -4,6 +4,7 @@ const {check} = require("express-validator");
 const Reservation = require('../models/Evento');
 const Habitacion = require('../models/Habitacion');
 const Cliente = require('../models/Cliente');
+const moment = require('moment');
 
 const getViewValidators = [
     check('')
@@ -82,30 +83,66 @@ async function showInstructionsView(req, res, next) {
     const uuid = req.params.uuid;
 
     try {
-        const reservations = await Reservation.findOne();
+        const reservations = await Reservation.findOne().lean();
         const reservation = reservations.events.find(reservation => reservation._id.toString() === uuid.toString());
         if(!reservation){
             throw new NotFoundError('Reservation does not exist');
         }
 
         
-        const client = await Cliente.findById(reservation.client.toString());
+        const client = await Cliente.findById(reservation.client.toString()).lean();
         if(!client){
             throw new NotFoundError('Client does not exist');
         }
         
-        const chalets = await Habitacion.findOne();
+        const chalets = await Habitacion.findOne().lean();
         const chalet = chalets.resources.find(chalet => chalet._id.toString() === reservation.resourceId.toString());
         if(!chalet){
             throw new NotFoundError('Chalet does not exist');
         }
         
         const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
-        const arrivalDate = new Date(reservation.arrivalDate);
-        const departureDate = new Date(reservation.departureDate);
+        // const arrivalDate = new Date(reservation.arrivalDate);
+        // const departureDate = new Date(reservation.departureDate);
 
-        reservation.arrivalDate = arrivalDate.toISOString();
-        reservation.departureDate = departureDate.toISOString();
+        // reservation.arrivalDate = arrivalDate.toISOString();
+        // reservation.departureDate = departureDate.toISOString();
+
+        console.log("reservation arrival date: ", reservation.arrivalDate);
+        console.log("reservation departure date: ", reservation.departureDate);
+
+        console.log("reservation arrival date solo fecha: ", reservation.arrivalDate.toLocaleString("es-MX", {timezone: "America/Mexico_City"}));
+        console.log("reservation departure date solo fecha: ", reservation.departureDate.toLocaleString("es-MX", {timezone: "America/Mexico_City"}));
+
+
+        // const arrivalDate = moment(reservation.arrivalDate).tz("America/Mexico_City").format("DD-MM-YYYY");
+        // const departureDate = moment.utc(reservation.departureDate).startOf('day').tz("America/Mexico_City").format("DD-MM-YYYY");
+
+        // const arrivalTime = moment(chalet.others.arrivalTime).tz("America/Mexico_City").format("HH:mm");
+        // const departureTime = moment(chalet.others.departureTime).tz("America/Mexico_City").format("HH:mm");
+
+        // reservation.arrivalDate = `${arrivalDate} a las ${arrivalTime} `
+        // reservation.departureDate = `${departureDate} a las ${departureTime} `;
+
+        const dateArrivalDate = new Date(reservation.arrivalDate);
+        const dateDepartureDate = new Date(reservation.departureDate);
+        const dateReceivedDate = new Date(reservation.reservationDate);
+
+        const formattedArrivalDate = formatDateMX(dateArrivalDate);
+        const formattedDepartureDate = formatDateMX(dateDepartureDate);
+        const formattedReceivedDate = formatDateMX(dateReceivedDate);
+
+        console.log("Arrival Date:", formattedArrivalDate); // "08-11-2024"
+        console.log("Departure Date:", formattedDepartureDate); // "10-11-2024"
+
+
+        const arrivalTime = moment(chalet.others.arrivalTime).tz("America/Mexico_City").format("HH:mm");
+        const departureTime = moment(chalet.others.departureTime).tz("America/Mexico_City").format("HH:mm");
+
+        reservation.arrivalDate = `${formattedArrivalDate} a las ${arrivalTime}`
+        reservation.departureDate = `${formattedDepartureDate} a las ${departureTime}`;
+
+        reservation.reservationDate = formattedReceivedDate;
 
         console.log("Fechassss: ")
 
@@ -115,12 +152,21 @@ async function showInstructionsView(req, res, next) {
         res.render('paraUsuarios', {
             layout: 'layoutParaUsuarios',
             fullname: client.firstName + ' ' + client.lastName,
-            reservation: reservation.toObject(),
-            chalet: chalet.toObject()
+            reservation: reservation,
+            chalet: chalet
         });
     } catch(err) {
         return next(err);
     }
+}
+
+function formatDateMX(date) {
+    const day = date.getUTCDate();
+    const monthNames = ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"];
+    const month = monthNames[date.getUTCMonth()];
+    const year = date.getUTCFullYear();
+    
+    return `${day} de ${month} de ${year}`;
 }
 
 async function areTermsAccepted(req, res, next) {
