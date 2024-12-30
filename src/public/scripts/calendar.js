0ecd3514ded462f911ffc8ae83c049d025ae89ef
xEfren548 <efren.alvarez@iteso.mx>
2024-12-30 12:03:55 -0600

    color naranja reservas

--- src/public/scripts/calendar.js
+++ src/public/scripts/calendar.js
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             } else if (clientPayments >= info.event.extendedProps.total) {
                 colorRectanguloTop = 'bg-success'
             } else {
-                colorRectanguloTop = 'bg-info'
+                colorRectanguloTop = 'background-orange'
             }
 
             if (madeCheckIn) {
