c3c534f8cd827f5792cc566e3b432fa3abf3ae78
xEfren548 <efren.alvarez@iteso.mx>
2025-01-10 14:33:07 -0600

    fix por depo

--- models/Cliente.js
+++ models/Cliente.js
@@ -17,6 +17,8 @@ const clientSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
+        sparse: true
     },
     identificationType: {
         type: String,
