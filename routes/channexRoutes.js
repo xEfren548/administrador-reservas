b29f193cb2ef420840b22f9cc0d51623edc848b6
xEfren548 <efren.alvarez@iteso.mx>
2025-05-30 16:41:37 -0600

    channex mgmt

--- routes/channexRoutes.js
+++ routes/channexRoutes.js
@@ -4,8 +4,25 @@ const channexController = require('../controllers/channexController');
 
 //   /api/channex/
 
+router.get('/home', (req, res) => {
+    res.render('homeChannex', {
+        layout: 'mainChannex',
+        error: req.query.error 
+    });
+});
 router.get('/properties', channexController.mapProperties);
 router.post('/properties/:id/create', channexController.createChannexProperty);
+
+// Dashboard (requiere estar conectado)
+router.get('/dashboard', async (req, res) => {
+    if (!req.session.channelId) return res.redirect('/?error=Debe+conectar+Airbnb');
+
+    const propiedades = await Habitacion.find().lean();
+    const chProps = await channexCtrl.getProperties(req.session.channelId);
+
+    res.render('dashboard', { propiedades, chProps });
+});
+
 router.post('/webhooks', channexController.webhookReceptor);
 router.get('/connect/airbnb', channexController.airbnbConnection);
 router.get('/auth/airbnb/callback', channexController.oauthAirbnb);
