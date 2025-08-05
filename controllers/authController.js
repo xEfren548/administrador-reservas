42afca25ed570b95c82765c9f59c91103bb94e06
Efren Dell <efren.am548@gmail.com>
2025-08-04 22:57:01 -0600

    token func

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,6 +36,7 @@ const validators = [
 
 async function login(req, res, next){
     const { email, password } = req.body;
+    console.log("Login attempt with email:", email);
 
     try{
         const user = await Usuario.findOne({ email });
@@ -94,6 +95,69 @@ async function login(req, res, next){
     }
 }
 
+async function loginToken(req, res, next){
+    const { email, password } = req.body;
+    console.log("Login attempt with email:", email);
+
+    try{
+        const user = await Usuario.findOne({ email });
+        if(!user){
+            return next(new BadRequestError("Wrong credentials"));
+        }
+
+        const pwdEqual = await bcrypt.compare(password, user.password);
+        if(!pwdEqual){
+            return next(new BadRequestError("Wrong credentials"));
+        }
+
+        // Generating authentiation token.
+        const token = jwt.sign({ email, userId: user._id }, "secret_key", { expiresIn: "24h" });
+        
+        // Saving user's cookies.
+        req.session = { 
+            token,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            privilege: user.privilege,
+            id: user._id,
+            privilege: user.privilege,
+            userId: user._id.toString(),
+            profileImageUrl: user.profileImageUrl,
+            role: user.role,
+            assignedChalets: user.assignedChalets
+        };        
+        
+        console.log("Usuario logeado con éxito");
+        // Uncomment the following line in order to test it on the browser.
+        console.log(req.session);
+        const redirectToCalendar = ['Administrador', 'Vendedor']
+        const redirectToTheirChalets = ['Dueño de cabañas', 'Colaborador de dueño de cabañas']
+        const redirectToUtilities = ['Inversionistas', 'Limpieza', 'Servicios adicionales']
+
+        const userPrivilege = req.session.privilege;
+
+        // if (redirectToCalendar.includes(userPrivilege)){
+        //     res.redirect('/');
+        // } else if(redirectToTheirChalets.includes(userPrivilege)){
+        //     res.redirect('/api/calendar/duenos')
+        // } else if(redirectToUtilities.includes(userPrivilege)){
+        //     res.redirect('/api/mostrar-utilidades')
+        // } else {
+        //     res.redirect('/api/dashboard');
+
+        // }
+
+        res.json({token});
+        
+        
+        // Uncomment the following line in order to test it on Postman.
+        //res.status(200).json( req.session );
+    } catch(err){
+        return next(err);
+    }
+}
+
 async function logout(req, res, next){
     req.session = null;
     console.log("Successfully logged out")
@@ -103,6 +167,7 @@ async function logout(req, res, next){
 module.exports = {
     validators,
     login,
+    loginToken,
     logout
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 const validationRequest = require('../common/middlewares/validation-request');
 
 router.post("/auth/login", /*authController.validators, validationRequest,*/ authController.login);
+router.post("/auth/login-token", authController.loginToken);
 router.all("/auth/logout", authController.logout);
 
 module.exports = router;
